25921e335b22648a5c15e3057111ebcce25304b7
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-10-29 02:55:35 -0500

    Define ToneCurve

--- src/types/pipeline/stage/tone_curve.rs
+++ src/types/pipeline/stage/tone_curve.rs
@@ -1,6 +1,6 @@
 use crate::types::ToneCurve;
 
-pub struct ToneCurvesData<'a> {
+pub struct ToneCurvesData<'a, 'b, 'c, 'd, 'e, 'f, 'g> {
     pub n_curves: u32,
-    pub the_curves: &'a [&'a ToneCurve],
+    pub the_curves: &'a [&'b ToneCurve<'c, 'd, 'e, 'f, 'g>],
 }
--- src/types/tone_curve.rs
+++ src/types/tone_curve.rs
@@ -1,7 +1,13 @@
-use crate::Context;
+use crate::plugin::ParametricCurveEvaluator;
 
 use super::{CurveSegment, InterpParams};
 
-pub struct ToneCurve {
-    pub(crate) context_id: Context,
+pub struct ToneCurve<'a, 'b, 'c, 'd, 'e> {
+    pub(crate) interp_params: InterpParams<u16>,
+    pub(crate) n_segments: usize,
+    pub(crate) segments: &'a [CurveSegment<'b>],
+    pub(crate) seg_interp: &'c [InterpParams<f32>],
+    pub(crate) evals: &'d [ParametricCurveEvaluator],
+    pub(crate) n_entries: usize,
+    pub(crate) table_16: &'e [u16],
 }

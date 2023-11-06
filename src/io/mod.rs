1fd03763f8788fecab0fdc4fd51f0a8298771f2b
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-11-05 20:07:00 -0600

    Make open_io_handler_from_file accessible

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -5,6 +5,7 @@ mod io_handler;
 mod mem;
 mod null;
 
+pub use file::open_io_handler_from_file;
 pub use io_handler::IoHandler;
 pub use mem::{open_io_handler_from_mem_for_reading, open_io_handler_from_mem_for_writing};
 pub use null::open_io_handler_from_null;
